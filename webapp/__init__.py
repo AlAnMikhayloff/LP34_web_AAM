b43f9b6fc03f3596b1a73fc39ed27ee482c3a9f1
AlAnMikhayloff <alanmikhayloff@gmail.com>
2024-10-16 01:03:16 +0300

    Развитие проекта и учёба

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,16 +1,25 @@
 from flask import Flask, render_template
 
+from webapp.model import db
+from webapp.forms import LoginForm
 from webapp.python_org_news import get_python_news
 from webapp.weather import weather_by_city 
 
 
 def create_app():
     app = Flask(__name__)
+    db.init_app(app)
 
     @app.route('/')
     def index():
         title = 'Классификатор'
         weather = weather_by_city('Tula,Russia')
-        news_list = get_python_news()
+        news_list = get_python_news('html')
         return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
+
+    @app.route('/login')
+    def login():
+        title = 'Авторизация'
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
     return app
--- webapp/config.py
+++ webapp/config.py
@@ -1,2 +1,8 @@
+import os 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 WEATHER_DEFAULT_CITY = "Tula,Russia"
-WEATHER_API_KEY = "5ae665c25830452face211650240810" 
+WEATHER_API_KEY = "5ae665c25830452face211650240810"
+
+SECRET_KEY = "owirhOIiHvioOIo*&KHUIHtKJHG)OIhuyut>?<MuhuYGBKJH^^%&^"
