66480c6e1da310fd4cfd80c45bb0d786f48a42d9
AlAnMikhayloff <alanmikhayloff@gmail.com>
2024-10-17 22:12:26 +0300

    Add config.py

--- webapp/config.py
+++ webapp/config.py
@@ -4,5 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 WEATHER_DEFAULT_CITY = "Tula,Russia"
 WEATHER_API_KEY = "5ae665c25830452face211650240810"
-
+WEATHER_URL = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
 SECRET_KEY = "owirhOIiHvioOIo*&KHUIHtKJHG)OIhuyut>?<MuhuYGBKJH^^%&^"
