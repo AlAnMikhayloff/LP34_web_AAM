0a424700e728e819ab44d6297783d8bf004cb025
AlAnMikhayloff <alanmikhayloff@gmail.com>
2024-10-23 23:48:41 +0300

    Добавлен функционал запоминания пользователя и blueprints

--- webapp/config.py
+++ webapp/config.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 import os 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -7,3 +8,4 @@ WEATHER_API_KEY = "5ae665c25830452face211650240810"
 WEATHER_URL = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
 SECRET_KEY = "owirhOIiHvioOIo*&KHUIHtKJHG)OIhuyut>?<MuhuYGBKJH^^%&^"
+REMEMBER_COOKIE_DURATION = timedelta(days=5)
---
+++ webapp/news/views.py
@@ -0,0 +1,12 @@
+from webapp.news.models import News
+from webapp.weather import weather_by_city 
+from flask import Blueprint, render_template, current_app
+
+blueprint = Blueprint('news', __name__)
+
+@blueprint.route('/')
+def index():
+    title = 'Классификатор'
+    weather = weather_by_city(current_app.config['WEATHER_DEFAULT_CITY'])
+    news_list = News.query.order_by(News.published.desc()).all()
+    return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
